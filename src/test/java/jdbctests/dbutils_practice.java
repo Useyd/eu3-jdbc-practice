f27128b7a450569ebacf9f8f9e5bedf0c6d8d7ab
Useyd <hamhumyugioh@gmail.com>
2020-11-15 00:31:34 +0100

    dbutils_practice

--- src/test/java/jdbctests/dbutils_practice.java
+++ src/test/java/jdbctests/dbutils_practice.java
@@ -1,9 +1,50 @@
 package jdbctests;
 
+import org.testng.annotations.Test;
+import utilities.DBUtils;
+
+import java.util.List;
+import java.util.Map;
+
 public class dbutils_practice {
 
 
+    @Test
+    public void test1(){
 
+        //create connection
+        DBUtils.createConnection();
+
+        //using method to get result as a list of maps
+        List<Map<String, Object>> queryResult = DBUtils.getQueryResultMap("select * from departments");
+
+        //printing the result
+        for(Map<String,Object> map : queryResult){
+            System.out.println(map.toString());
+        }
+
+
+        //close connection
+        DBUtils.destroy();
+
+    }
+
+    @Test
+    public void test2(){
+
+        //create connection
+        DBUtils.createConnection();
+
+        //first wrote the DBUtils.getrowMap bla bla then for left side of the code,
+        //ctrl+alt+v for introduce local variable with shortcut
+        Map<String, Object> rowMap = DBUtils.getRowMap("select first_name, last_name, salary, job_id from employees where employee_id = 100");
+
+        System.out.println(rowMap.toString());
+
+        //close connection
+        DBUtils.destroy();
+
+    }
 
 
 }
